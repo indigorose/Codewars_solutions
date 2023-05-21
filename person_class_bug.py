dc225f78d58a9dadbf2b7799c5881c4e13b67c3e
indigorose <indigorose049@gmail.com>
2023-05-21 14:49:52 +0100

    Update kata solution

--- person_class_bug.py
+++ person_class_bug.py
@@ -4,6 +4,8 @@ class Person():
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
+        self.age = age
+        self.full_name = first_name + " " + last_name
 
-# The original code does not allow access to the person instance.
-# Modification to access person full name and age parameters.
+# The original code does not allow access to the person age and full name instance.
+# Refactor to access person age parameter and full name.
