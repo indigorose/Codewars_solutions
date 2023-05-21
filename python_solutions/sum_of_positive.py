d888d9c088b0769af3cb21e5c67270a116ef6471
indigorose <indigorose049@gmail.com>
2023-05-21 15:16:15 +0100

    Update kata solution

--- python_solutions/sum_of_positive.py
+++ python_solutions/sum_of_positive.py
@@ -1,6 +1,18 @@
 # Sum of Positive
-# Return the sum of all the positives in an array.
+# Return the sum of all the positives in an array/list.
 
 def positive_sum(arr):
     # Your code here
-    pass
+    result = 0
+    for a in arr:
+        if a >= 0:
+            result += a
+        else:
+            pass
+    return result
+
+# Alternative Solution
+# return sum(a for a in arr if a > 0)
+
+
+print(positive_sum([1, 2, 3, 4, 5]))
