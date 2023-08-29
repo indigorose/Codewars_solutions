6f5bc48b12dc5b97a59f6b2eac5f01dba72da58d
Karen Robertson <70444332+indigorose@users.noreply.github.com>
2023-08-29 14:33:34 +0100

    Update completed katas

--- main.py
+++ main.py
@@ -41,7 +41,17 @@ def solution(s):
     
     
 # Add Nato dictionary LETTERS with capitalised keys 
-
+LETTERS = {
+    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",   "H": "Hotel",   "I": "India",
+    "J": "Juliett","K": "Kilo",    "L": "Lima",
+    "M": "Mike",   "N": "November","O": "Oscar",
+    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra", "T": "Tango",   "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu" }
+  
 def nato(word):
     return " ".join([LETTERS[i.upper()] for i in word])
         
