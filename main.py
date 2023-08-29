cc6bd5e14d10bdb2945287969abcfaefcda71254
indigorose <indigorose049@gmail.com>
2023-08-29 17:00:36 +0100

    Update spelling

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ LETTERS = {
     "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
     "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
     "G": "Golf",   "H": "Hotel",   "I": "India",
-    "J": "Juliett", "K": "Kilo",    "L": "Lima",
+    "J": "Juliet", "K": "Kilo",    "L": "Lima",
     "M": "Mike",   "N": "November", "O": "Oscar",
     "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
     "S": "Sierra", "T": "Tango",   "U": "Uniform",
