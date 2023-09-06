3846a7d613a7a7c62ef243d455887b97e60e42cd
Karen Robertson <70444332+indigorose@users.noreply.github.com>
2023-09-06 13:39:18 +0100

    Update completed katas

--- python_solutions/app_main.py
+++ python_solutions/app_main.py
@@ -3,3 +3,8 @@ def minimum(arr):
 
 def maximum(arr):
     return max(arr)
+    
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    #Happy Coding! ;)
+    return True if fuel_left >= (distance_to_pump/mpg) else False
+
