c1d9d0b2d924ad2ea415fa4181361f864c3eb195
Karen Robertson <70444332+indigorose@users.noreply.github.com>
2023-09-14 08:24:25 +0100

    Update completed kata

--- python_solutions/app_main.py
+++ python_solutions/app_main.py
@@ -10,4 +10,24 @@ def zero_fuel(distance_to_pump, mpg, fuel_left):
     
 def return_five():
     return len('hello')
+    
+def get_planet_name(id):
+    # This doesn't work; Fix it!
+    match id:
+        case 1:
+            return "Mercury"
+        case 2:
+            return "Venus"
+        case 3: 
+            return "Earth"
+        case 4: 
+            return "Mars"
+        case 5: 
+            return "Jupiter"
+        case 6: 
+            return "Saturn"
+        case 7: 
+            return "Uranus"  
+        case 8:
+            return "Neptune"
 
