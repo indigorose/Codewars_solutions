0496ae0efc740e887eb34219d8ab065ec0d7aae0
indigorose <indigorose049@gmail.com>
2023-09-21 20:39:58 +0100

    Update kata solutions

--- python_solutions/app_main.py
+++ python_solutions/app_main.py
@@ -1,16 +1,20 @@
 def minimum(arr):
     return min(arr)
 
+
 def maximum(arr):
     return max(arr)
-    
+
+
 def zero_fuel(distance_to_pump, mpg, fuel_left):
-    #Happy Coding! ;)
-    return True if fuel_left >= (distance_to_pump/mpg) else False
-    
+    # Happy Coding! ;)
+    return True if fuel_left >= (distance_to_pump / mpg) else False
+
+
 def return_five():
     return len('hello')
-    
+
+
 def get_planet_name(id):
     # This doesn't work; Fix it!
     match id:
@@ -18,51 +22,49 @@ def get_planet_name(id):
             return "Mercury"
         case 2:
             return "Venus"
-        case 3: 
+        case 3:
             return "Earth"
-        case 4: 
+        case 4:
             return "Mars"
-        case 5: 
+        case 5:
             return "Jupiter"
-        case 6: 
+        case 6:
             return "Saturn"
-        case 7: 
-            return "Uranus"  
+        case 7:
+            return "Uranus"
         case 8:
             return "Neptune"
-            
+
 
 def small_enough(array, limit):
     for i in array:
         if i > limit:
             return False
     return True
+
+
 # alternative max(arr) <= limit
 def capitals(word):
     return [word.index(i) for i in word if i.isupper()]
-    
+
+
 def hex_to_dec(s):
     return int(s, 16)
-    
+
+
 def update_light(current):
     match current:
         case "green":
             return "yellow"
-        case 'yellow': 
-            return"red"
+        case 'yellow':
+            return "red"
         case 'red':
             return 'green'
-            
-            
-            
+
+
 def combat(health, damage):
     if health - damage > 0:
         health -= damage
         return health
     else:
-        return  0
-
-
-
-
-    
+        return 0
