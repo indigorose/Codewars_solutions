4e7d23ef056fac4d91e6ed5d61979c68673e6130
indigorose <indigorose049@gmail.com>
2023-09-25 18:54:35 +0100

    Update completed katas

---
+++ python_solutions/more_than_zero.py
@@ -0,0 +1,14 @@
+def corrections(x):
+    if x > 0:
+        return f'{x} is more than zero.'
+    else:
+        return f'{x} is equal to or less than zero.'
+
+
+''' 
+before corrections
+def corrections(x,y,z
+    elif x > 0
+        append float(x) + (is more than zero."
+    elsse
+        retorn float(x) + "is equal to or less than zero.)'''
