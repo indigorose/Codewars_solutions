a84dba3027f6e859d056fffdd365fe21e85ac8d7
Karen Robertson <70444332+indigorose@users.noreply.github.com>
2023-10-29 08:04:28 +0000

    Update app_main.py

--- python_solutions/app_main.py
+++ python_solutions/app_main.py
@@ -208,3 +208,14 @@ def unique_in_order(sequence):
 def odd_or_even(arr):
     return 'even'if sum(arr) % 2 == 0 else 'odd'
 
+
+def get_even_numbers(arr):
+    return list(filter(lambda x: x % 2 ==0,arr))
+
+
+
+
+
+
+
+
