1b5630da486ccb425707cc6c81fd38364e9cdab4
Karen Robertson <70444332+indigorose@users.noreply.github.com>
2023-10-29 15:06:58 +0000

    Update app_main.py

--- python_solutions/app_main.py
+++ python_solutions/app_main.py
@@ -214,6 +214,16 @@ def get_even_numbers(arr):
 
 
 
+def xo(s):
+    x = 0
+    o = 0
+    for i in s.lower():
+        if i == 'x':
+            x+=1
+        elif i == 'o':
+            o+=1
+    return x==o
+
 
 
 
