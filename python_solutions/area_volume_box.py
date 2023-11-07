6cf5a39c6ff711e7e27f3ce6f2c0aec678510e95
indigorose <indigorose049@gmail.com>
2023-11-07 18:19:14 +0000

    Update completed katas

---
+++ python_solutions/area_volume_box.py
@@ -0,0 +1,4 @@
+# Find the surface area and volume of a box.
+
+def get_size(w, h, d):
+    return [((2*(d*w))+(2*(d*h))+(2*(h*w))), (w*h*d)]
---
+++ python_solutions/sum_of_angles.py
@@ -0,0 +1,4 @@
+# Find the interior sum of angles for any polygon greater than 2 sides.
+
+def angle(n):
+    return (n-2) * 180
