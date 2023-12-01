b3b827920fe4b67732fc857a6564430262572a08
Karen Robertson <70444332+indigorose@users.noreply.github.com>
2023-12-01 19:45:55 +0000

    Update app_main.py

--- python_solutions/app_main.py
+++ python_solutions/app_main.py
@@ -246,4 +246,27 @@ def summation(num):
     #return "I like {1}!".format(",".join(args))
 
 
+list =["1abc2",
+  "pqr3stu8vwx", 
+  "a1b2c3d4e5f",  
+"treb7uchet"]
 
+def sum_list(list):
+  result = []
+  y = ""
+  count = 0
+  for x in list:
+    y = re.sub(r"[^\d]", "", x)
+    result.append(y)
+  print(result)
+  for i in result:
+    if len(i) <= 1: 
+      j = i+i
+      print(j)
+      count += int(j)
+    else:
+      k =(i[0]) + (i[-1])
+      print(k)
+      count += int(k)
+  return count
+print(sum_list(list))
