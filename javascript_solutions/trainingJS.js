3843272f0d7d739d922f3f4cb8fad3e19e1bc984
indigorose <indigorose049@gmail.com>
2024-01-02 13:50:30 +0000

    Update completed kata

--- javascript_solutions/trainingJS.js
+++ javascript_solutions/trainingJS.js
@@ -44,61 +44,75 @@ function equal5() {
 	return a % b;
 }
 
-
 // Training JS #3: Basic data types--String
 
-var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
-function Dad(){
-  //select some variable to combine "Dad"
-  return d1+a2+d2;
+var a1 = 'A',
+	a2 = 'a',
+	b1 = 'B',
+	b2 = 'b',
+	c1 = 'C',
+	c2 = 'c',
+	d1 = 'D',
+	d2 = 'd',
+	e1 = 'E',
+	e2 = 'e',
+	n1 = 'N',
+	n2 = 'n';
+function Dad() {
+	//select some variable to combine "Dad"
+	return d1 + a2 + d2;
 }
-function Bee(){
-  //select some variable to combine "Bee"
-  return b1+e2+e2;
+function Bee() {
+	//select some variable to combine "Bee"
+	return b1 + e2 + e2;
 }
-function banana(){
-  //select some variable to combine "banana"
-  return b2+a2+n2+a2+n2+a2;
+function banana() {
+	//select some variable to combine "banana"
+	return b2 + a2 + n2 + a2 + n2 + a2;
 }
 
 //answer some questions if you finished works above
-function answer1(){
-  //the answer should be "yes" or "no"
-  return "no";
+function answer1() {
+	//the answer should be "yes" or "no"
+	return 'no';
 }
-function answer2(){
-  //the answer should be "yes" or "no"
-  return "no";
+function answer2() {
+	//the answer should be "yes" or "no"
+	return 'no';
 }
-function answer3(){
-  //the answer should be "yes" or "no"
-  return "yes";
+function answer3() {
+	//the answer should be "yes" or "no"
+	return 'yes';
 }
 
 // Training JS #4: Basic data types--Arrays
 
-function getLength(arr){
-  //return length of arr
-  return arr.length
+function getLength(arr) {
+	//return length of arr
+	return arr.length;
 }
-function getFirst(arr){
-  //return the first element of arr
-  return arr[0]
+function getFirst(arr) {
+	//return the first element of arr
+	return arr[0];
 }
-function getLast(arr){
-  //return the last element of arr
-  return arr[arr.length-1]
+function getLast(arr) {
+	//return the last element of arr
+	return arr[arr.length - 1];
 }
-function pushElement(arr){
-  var el=1;
-  //push el to arr
-  arr.push(el)
-  return arr
+function pushElement(arr) {
+	var el = 1;
+	//push el to arr
+	arr.push(el);
+	return arr;
 }
-function popElement(arr){
-  //pop an element from arr
-  arr.pop()
-  return arr
+function popElement(arr) {
+	//pop an element from arr
+	arr.pop();
+	return arr;
 }
 
+//  Training JS #5: Basic data types--Object
 
+function animal(obj) {
+	return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
