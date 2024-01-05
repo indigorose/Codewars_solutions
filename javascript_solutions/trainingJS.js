25ebfc66f6d34e0c04b899ef63f201ce187acff0
indigorose <indigorose049@gmail.com>
2024-01-05 18:28:17 +0000

    Update completed kata

--- javascript_solutions/trainingJS.js
+++ javascript_solutions/trainingJS.js
@@ -126,3 +126,48 @@ function trueOrFalse(val) {
 		return 'true';
 	}
 }
+
+// Training JS #8: Conditional statement--switch
+
+function howManydays(month) {
+	var days = 0;
+	switch (month) {
+		case 1:
+			return 31;
+			break;
+		case 2:
+			return 28;
+			break;
+		case 3:
+			return 31;
+			break;
+		case 4:
+			return 30;
+			break;
+		case 5:
+			return 31;
+			break;
+		case 6:
+			return 30;
+			break;
+		case 7:
+			return 31;
+			break;
+		case 8:
+			return 31;
+			break;
+		case 9:
+			return 30;
+			break;
+		case 10:
+			return 31;
+			break;
+		case 11:
+			return 30;
+			break;
+		case 12:
+			return 31;
+			break;
+	}
+	return days;
+}
