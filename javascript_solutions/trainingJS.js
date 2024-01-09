398edf4094239cec6bdb94f330383e6950904398
indigorose <indigorose049@gmail.com>
2024-01-09 21:58:09 +0000

    Update completed kata

--- javascript_solutions/trainingJS.js
+++ javascript_solutions/trainingJS.js
@@ -188,3 +188,19 @@ function padIt(str, n) {
 	}
 	return result;
 }
+
+// Training Js #10: Loop statement--for
+
+function pickIt(arr) {
+	var odd = [],
+		even = [];
+	for (i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 0) {
+			even.push(arr[i]);
+		} else {
+			odd.push(arr[i]);
+		}
+	}
+
+	return [odd, even];
+}
