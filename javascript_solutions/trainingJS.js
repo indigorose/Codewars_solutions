a79ccf69b281bbff03ab5c22940ad4ca7d27704b
indigorose <indigorose049@gmail.com>
2024-01-11 22:11:42 +0000

    Update completed kata

--- javascript_solutions/trainingJS.js
+++ javascript_solutions/trainingJS.js
@@ -204,3 +204,22 @@ function pickIt(arr) {
 
 	return [odd, even];
 }
+
+// Training JS #11: loop statement -- break, continue
+function grabDoll(dolls) {
+	var bag = [];
+	for (var i = 0; i < dolls.length; i++) {
+		if (dolls[i] == 'Hello Kitty' || dolls[i] == 'Barbie doll') {
+			bag.push(dolls[i]);
+			if (bag.length == 3) {
+				break;
+			} else {
+				continue;
+			}
+		} else {
+			continue;
+		}
+	}
+
+	return bag;
+}
