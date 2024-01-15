500636851988010a8ae56f1b60a094aa436a5164
indigorose <indigorose049@gmail.com>
2024-01-15 21:11:29 +0000

    Update completed kata

--- javascript_solutions/app.js
+++ javascript_solutions/app.js
@@ -2,3 +2,19 @@ function take(arr, n) {
 	// Your code here
 	return arr.slice(0, n);
 }
+
+function whatNumberIsIt(n) {
+	if (n == Number.MIN_VALUE) {
+		return 'Input number is Number.MIN_VALUE';
+	} else if (n == -Infinity) {
+		return 'Input number is Number.NEGATIVE_INFINITY';
+	} else if (n == Infinity) {
+		return 'Input number is Number.POSITIVE_INFINITY';
+	} else if (n == Number.MAX_VALUE) {
+		('Input number is Number.MAX_VALUE');
+	} else if (n == Number(n)) {
+		return `Input number is ${n}`;
+	} else {
+		return 'Input number is Number NaN';
+	}
+}
--- javascript_solutions/trainingJS.js
+++ javascript_solutions/trainingJS.js
@@ -242,3 +242,21 @@ function giveMeFive(obj) {
 	}
 	return arr;
 }
+
+// Training JS #13: Number object and its properties
+
+function whatNumberIsIt(n) {
+	if (n == Number.MAX_VALUE) {
+		return 'Input number is Number.MAX_VALUE';
+	} else if (n == Number.MIN_VALUE) {
+		return 'Input number is Number.MIN_VALUE';
+	} else if (n == -Infinity) {
+		return 'Input number is Number.NEGATIVE_INFINITY';
+	} else if (n == Infinity) {
+		return 'Input number is Number.POSITIVE_INFINITY';
+	} else if (n == Number(n)) {
+		return `Input number is ${n}`;
+	} else {
+		return 'Input number is Number.NaN';
+	}
+}
