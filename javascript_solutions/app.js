9a6ec7585906034ef5a853096428f3fee1a67e0c
indigorose <indigorose049@gmail.com>
2024-01-21 15:42:23 +0000

    Update completed kata

--- javascript_solutions/app.js
+++ javascript_solutions/app.js
@@ -59,4 +59,26 @@ function alienLanguage(str) {
 	return words.join(' ');
 }
 
-console.log(alienLanguage('My name is example'));
+// console.log(alienLanguage('My name is example'));
+
+function sumOfDifferences(arr) {
+	if (arr.length >= 2) {
+		arr.sort(function (a, b) {
+			return b - a;
+		});
+	} else {
+		return 0;
+	}
+	let result = 0;
+	for (i = 0; i < arr.length - 1; i++) {
+		if (arr[i + 1] == arr[arr.length - 1]) {
+			result += arr[i] - arr[i + 1];
+		} else {
+			console.log(result);
+			result += arr[i] - arr[i + 1];
+		}
+	}
+	return result;
+}
+
+console.log(sumOfDifferences([1, 2, 10]));
---
+++ javascript_solutions/sum_of_differences.js
@@ -0,0 +1,19 @@
+function sumOfDifferences(arr) {
+	if (arr.length >= 2) {
+		arr.sort(function (a, b) {
+			return b - a;
+		});
+	} else {
+		return 0;
+	}
+	let result = 0;
+	for (i = 0; i < arr.length - 1; i++) {
+		if (arr[i + 1] == arr[arr.length - 1]) {
+			result += arr[i] - arr[i + 1];
+		} else {
+			console.log(result);
+			result += arr[i] - arr[i + 1];
+		}
+	}
+	return result;
+}
