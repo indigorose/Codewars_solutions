ca5dfb035e4850479401962e846e1ee3483f2668
indigorose <indigorose049@gmail.com>
2024-01-30 21:30:37 +0000

    Update completed kata

--- javascript_solutions/app.js
+++ javascript_solutions/app.js
@@ -124,3 +124,13 @@ function well(x) {
 }
 
 console.log(well(['bad', 'bad', 'bad']));
+
+function divisibleBy(numbers, divisor) {
+	result = [];
+	for (let i = 0; i < numbers.length; i++) {
+		if (numbers[i] % divisor == 0) {
+			result.push(numbers[i]);
+		}
+	}
+	return result;
+}
