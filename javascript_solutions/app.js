2bdb1b64c7e95940729461ca898f3f465bf78251
indigorose <indigorose049@gmail.com>
2024-02-08 06:50:08 +0000

    Update completed kata

--- javascript_solutions/app.js
+++ javascript_solutions/app.js
@@ -169,3 +169,27 @@ function oddOnesOut(nums) {
 }
 
 console.log(oddOnesOut([1, 2, 1, 3, 3, 3]));
+
+function add(a, b) {
+	return a + b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function mod(a, b) {
+	return a % b;
+}
+
+function exponent(a, b) {
+	return a ** b;
+}
+
+function subt(a, b) {
+	return a - b;
+}
