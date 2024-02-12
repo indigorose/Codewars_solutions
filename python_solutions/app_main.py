6dd33766bd2f3f50fa58bc18b2523111c6cb4178
indigorose <indigorose049@gmail.com>
2024-02-12 20:11:40 +0000

    Update completed kata

--- python_solutions/app_main.py
+++ python_solutions/app_main.py
@@ -295,3 +295,23 @@ def testit(string):
 
 # def testit(s):
 #     return s[::-1].title()[::-1]
+
+
+def whatday(num):
+    match num:
+        case 1:
+            return 'Sunday'
+        case 2:
+            return 'Monday'
+        case 3:
+            return 'Tuesday'
+        case 4:
+            return 'Wednesday'
+        case 5:
+            return 'Thursday'
+        case 6:
+            return 'Friday'
+        case 7:
+            return 'Saturday'
+        case _:
+            return 'Wrong, please enter a number between 1 and 7'
