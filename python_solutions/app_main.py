a9b484db340aab52948b5edc1b13e145e397a932
indigorose <indigorose049@gmail.com>
2024-02-28 21:26:13 +0000

    Update complete kata

--- python_solutions/app_main.py
+++ python_solutions/app_main.py
@@ -391,8 +391,10 @@ def greet_(language):
     return next((i[1] for i in database if i[0] == language), 'Welcome')
 
 
-
 def sale_hotdogs(n):
-    return 100*n if n < 5 else n*95 if 5 <= n<10 else 90*n
+    return 100*n if n < 5 else n*95 if 5 <= n < 10 else 90*n
 
 
+def largest_pair_sum(numbers):
+    numbers.sort()
+    return numbers[-2] + numbers[-1]
