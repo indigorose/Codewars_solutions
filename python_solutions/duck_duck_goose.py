ee10d55ecf4e9cf554fda4242659d1956baf688e
indigorose <indigorose049@gmail.com>
2024-03-21 22:54:17 +0000

    Update completed kata

---
+++ python_solutions/duck_duck_goose.py
@@ -0,0 +1,8 @@
+def duck_duck_goose(players, goose):
+    if goose < len(players):
+        return players[goose-1].name
+    elif goose % len(players) == 0:
+        return players[len(players)-1].name
+    else:
+        x = int(goose % len(players) - (len(players)+1))
+        return players[x].name
