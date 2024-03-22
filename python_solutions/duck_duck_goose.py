98f38478bea2025d4758485aa02bf3bda1bf9744
indigorose <indigorose049@gmail.com>
2024-03-22 17:36:03 +0000

    Update kata notes

--- python_solutions/duck_duck_goose.py
+++ python_solutions/duck_duck_goose.py
@@ -6,3 +6,6 @@ def duck_duck_goose(players, goose):
     else:
         x = int(goose % len(players) - (len(players)+1))
         return players[x].name
+
+
+# return players[(goose % len(players)) - 1].name
