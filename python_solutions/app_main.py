4676f472dbc6a1f9d651e285dab8f81316d47256
Karen Robertson <70444332+indigorose@users.noreply.github.com>
2024-04-05 21:10:13 +0100

    Update app_main.py

--- python_solutions/app_main.py
+++ python_solutions/app_main.py
@@ -496,3 +496,15 @@ def even_numbers(arr, n):
 
 def print_array(arr):
     return ",".join([str(i) for i in arr])
+
+
+
+def power_of_two(x):
+    if (x == 0):
+        return False
+    while (x != 1):
+        if (x % 2 != 0):
+            return False
+        x = x // 2
+    return True
+
