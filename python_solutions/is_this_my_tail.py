bcf300104052a5f02627990728f384d58066e26a
indigorose <indigorose049@gmail.com>
2024-06-09 21:25:22 +0100

    Update completed kata

---
+++ python_solutions/is_this_my_tail.py
@@ -0,0 +1,8 @@
+# def correct_tail(body, tail):
+#      sub = body.substr(len(body)-len(tail.length)
+#         if sub=tai:
+#     return True
+#         else:
+#     return False
+def correct_tail(body, tail):
+    return body[-1] == tail
