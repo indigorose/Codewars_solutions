83dc13092dc0b32441f823106923be097f2955a7
Karen Robertson <70444332+indigorose@users.noreply.github.com>
2024-07-04 20:26:53 +0100

    Update app.js

--- javascript_solutions/app.js
+++ javascript_solutions/app.js
@@ -268,3 +268,11 @@ function findOdd(A) {
 		}
 	}
 }
+
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+   return dna.replaceAll("T", "U")
+
+}
+
+
