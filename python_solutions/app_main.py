d644e66f831e7432dc77bd2eeed26e53d142b3ed
indigorose <indigorose049@gmail.com>
2024-07-30 22:37:51 +0100

    Update: completed kata

--- python_solutions/app_main.py
+++ python_solutions/app_main.py
@@ -644,3 +644,13 @@ def trees(height):
 
 def problem(a):
     return (a * 50) + 6 if type(a) != str else "Error"
+
+
+class Ship:
+    def __init__(self, draft, crew):
+        self.draft = draft
+        self.crew = crew
+    # Your code here
+
+    def is_worth_it(self):
+        return self.draft - self.crew * 1.5 > 20
