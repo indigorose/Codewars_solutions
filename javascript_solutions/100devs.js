5cd42108b6b796015327cca81119d73da4735ead
indigorose <indigorose049@gmail.com>
2024-09-07 23:20:46 +0100

    Update: completed kata

--- javascript_solutions/100devs.js
+++ javascript_solutions/100devs.js
@@ -13,3 +13,13 @@ let cc = '4556364607935616';
 // let lastFour = cc.slice(-4);
 
 console.log(maskify(cc));
+
+function getSum(a, b) {
+	let min = Math.min(a, b);
+	let max = Math.max(a, b);
+	let sum = 0;
+	for (let i = min; i <= max; i++) {
+		sum += i;
+	}
+	return sum;
+}
