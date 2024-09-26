29375bb4891c2dbea0d671097a43228763fd0fc1
indigorose <indigorose049@gmail.com>
2024-09-26 22:28:57 +0100

    Update: javascript kata

--- javascript_solutions/app.js
+++ javascript_solutions/app.js
@@ -330,3 +330,25 @@ function howManyLightsabersDoYouOwn(name) {
 function minMax(arr) {
 	return [Math.min(...arr), Math.max(...arr)]; // fix me!
 }
+
+function grader(score) {
+	if (score >= 0.9 && score <= 1) {
+		return 'A';
+	} else if (score >= 0.8 && score < 0.9) {
+		return 'B';
+	} else if (score >= 0.7 && score < 0.8) {
+		return 'C';
+	} else if (score >= 0.6 && score < 0.7) {
+		return 'D';
+	} else {
+		return 'F';
+	}
+}
+
+function grade(score) {
+	if (score > 1 || score < 0.6) return 'F';
+	if (score < 0.7) return 'D';
+	if (score < 0.8) return 'C';
+	if (score < 0.9) return 'B';
+	return 'A';
+}
