ca152674a59f0b5547ff9e8a7faaed47dd8fb5b4
indigorose <indigorose049@gmail.com>
2024-10-27 13:27:11 +0000

    Update: javascript kata

--- javascript_solutions/fcc_javascript.js
+++ javascript_solutions/fcc_javascript.js
@@ -7,3 +7,14 @@ const hand = { rock: 1, paper: 2, scissors: 3 };
 const result = ['Draw!', 'Player 1 won!', 'Player 2 won!'];
 const rps = (p1, p2) => {};
 console.log(result[hand['rock'] - hand['scissors']]);
+
+// Get the mean of an array
+
+function getAverage(marks) {
+	//TODO : calculate the downward rounded average of the marks array
+	let sum = 0;
+	for (let i = 0; i < marks.length; i++) {
+		sum += marks[i];
+	}
+	return Math.floor(sum / marks.length);
+}
