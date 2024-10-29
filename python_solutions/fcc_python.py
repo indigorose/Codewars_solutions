9fdc078f15fbeae50dfc572653cbd2e2a672771b
indigorose <indigorose049@gmail.com>
2024-10-29 22:28:15 +0000

    Update: completed kata

--- python_solutions/fcc_python.py
+++ python_solutions/fcc_python.py
@@ -2,3 +2,13 @@
 
 def divisible_by(numbers, divisor):
     return [i for i in numbers if i % divisor == 0]
+
+# return masked string
+
+
+def maskify(cc):
+    lastFour = cc[-4:]
+    if len(cc) <= 4:
+        return cc
+    else:
+        return '#' * (len(cc)-4) + lastFour
