22120cf22db93621c67158824812de8beb6350e0
indigorose <indigorose049@gmail.com>
2024-11-07 21:54:55 +0000

    Update completed kata

--- javascript_solutions/100devs.js
+++ javascript_solutions/100devs.js
@@ -42,3 +42,22 @@ console.log(twoSums([1, 2, 2, 3, 4], 4));
 
 // Test arr = [1, 2, 2, 3, 4], target = 4
 // Result  = [[2,2] ,[3,1]]
+
+function sumMix(x) {
+	const initialValue = 0;
+	const result = x.reduce(
+		(accumulator, currentValue) => accumulator + Number(currentValue),
+		initialValue
+	);
+	return result;
+}
+
+// A mix array of string and integers that are to sum to a result
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+// assert.strictEqual(sumMix([9, 3, '7', '3']), 22);
+// assert.strictEqual(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42);
+// assert.strictEqual(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41);
