05f9d894a84adb9d6440147321c054a34be4ee03
indigorose <indigorose049@gmail.com>
2025-08-27 20:18:44 +0100

    Update javascript solution

---
+++ javascript_solutions/fun_with_ES6_classes.js
@@ -0,0 +1,20 @@
+class Person {
+	// Get coding in ES6 :D
+	constructor(
+		firstName = 'John',
+		lastName = 'Doe',
+		age = 0,
+		gender = 'Male'
+	) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.gender = gender;
+	}
+	sayFullName() {
+		return `${this.firstName} ${this.lastName}`;
+	}
+	static greetExtraTerrestrials(raceName) {
+		return `Welcome to Planet Earth ${raceName}`;
+	}
+}
--- python_solutions/thonny_test.py
+++ python_solutions/thonny_test.py
@@ -11,5 +11,12 @@ def find_even_index(array):
     return -1
 
 
-print(find_even_index([1,100,50,-51,1,1]))
-print(find_even_index([1,2,3,4,3,2,1]))     
+print(find_even_index([1, 100, 50, -51, 1, 1]))
+print(find_even_index([1, 2, 3, 4, 3, 2, 1]))
+
+
+def find_even_index2(arr):
+    for i in range(len(arr)):
+        if sum(arr[:i]) == sum(arr[i+1:]):
+            return i
+    return -1
