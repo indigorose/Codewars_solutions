0bbd88b647499b8c15c085e899e6b31c6c99ddab
indigorose <indigorose049@gmail.com>
2026-03-02 21:57:08 +0000

    Update solution

---
+++ javascript_solutions/spam-filter/script.js
@@ -0,0 +1,12 @@
+const messageInput = document.getElementById('message-input');
+const result = document.getElementById('result-message');
+const checkMessageButton = document.getElementById('check-message-btn');
+
+const isSpam = (msg) => false;
+
+checkMessageButton.addEventListener('click', () => {
+	if (messageInput.value === '') {
+		alert('Please enter a message.');
+		return;
+	}
+});
