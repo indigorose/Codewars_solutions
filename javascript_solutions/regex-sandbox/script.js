3d3ce8a3389bebdbaad2229b97c1200165d2800f
indigorose <indigorose049@gmail.com>
2026-03-06 13:44:14 +0000

    Update solution

--- javascript_solutions/regex-sandbox/script.js
+++ javascript_solutions/regex-sandbox/script.js
@@ -7,11 +7,24 @@ const caseInsensitiveFlag = document.getElementById('i');
 const globalFlag = document.getElementById('g');
 
 const getFlags = () => {
-	if (caseInsensitiveFlag.checked) {
-		return 'i';
-	} else if (globalFlag.checked) {
-		return 'g';
-	} else {
+	if (caseInsensitiveFlag.checked && globalFlag.checked) {
+		console.log('ig clicked');
 		return 'ig';
+	} else if (globalFlag.checked) {
+		console.log('g clicked');
+		return 'g';
+	} else if (caseInsensitiveFlag.checked) {
+		console.log('i clicked');
+		return 'i';
+	} else {
+		console.log('empty sting');
+		return '';
 	}
 };
+
+testButton.addEventListener('click', () => {
+	getFlags();
+	const testRegex = regexPattern.value;
+	console.log(stringToTest.innerHTML);
+	console.log(testRegex);
+});
