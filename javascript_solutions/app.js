2a9979274e8ceeb3276ef0dd5bf62d74f8662252
Karen Robertson <70444332+indigorose@users.noreply.github.com>
2026-03-07 16:08:01 +0000

    Update app.js

--- javascript_solutions/app.js
+++ javascript_solutions/app.js
@@ -419,3 +419,18 @@ let Ghost = function() {
   let i = Math.floor(Math.random() * colors.length)
   this.color = colors[i]
   }
+		
+		
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
+
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
+}
+
