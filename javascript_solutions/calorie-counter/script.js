98401e58071e9619d87b3cb7379014606c48ce77
indigorose <indigorose049@gmail.com>
2026-03-12 19:46:42 +0000

    Update solution

--- javascript_solutions/calorie-counter/script.js
+++ javascript_solutions/calorie-counter/script.js
@@ -17,5 +17,30 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-	const targetId = '#' + entryDropdown.value;
+	const targetInputContainer = document.querySelector(
+		`#${entryDropdown.value} .input-container`,
+	);
+	const entryNumber =
+		targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+	const HTMLString = `
+	<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+	<input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+	<label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+	<input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+	/>`;
+	targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
+
+function getCaloriesFromInputs(list) {
+	let calories = 0;
+
+	for (const item of list) {
+		const currVal = cleanInputString(item.value);
+	}
+}
+
+addEntryButton.addEventListener('click', addEntry);
