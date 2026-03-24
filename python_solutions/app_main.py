93a785c57966c61ebbeca68ab0f1327fd25e4da8
Karen Robertson <70444332+indigorose@users.noreply.github.com>
2026-03-24 15:08:37 +0000

    Update app_main.py

--- python_solutions/app_main.py
+++ python_solutions/app_main.py
@@ -821,3 +821,10 @@ def swap_values(args):
 
 def player_rank_up(pts):
     return 'Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.' if pts >= 100 else False
+
+def passing_count(scores, passing_score):
+    count =0
+    for score in scores:
+        if score >= passing_score:
+            count +=1
+    return count
