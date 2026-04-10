e9cdbb6b633c51160baf788aedb6eedb03db3c6a
indigorose <indigorose049@gmail.com>
2026-04-10 22:04:38 +0100

    Update solution

--- javascript_solutions/recipe-ingredient-converter.js
+++ javascript_solutions/recipe-ingredient-converter.js
@@ -1,27 +1,56 @@
 const conversionTable = {
-  cup: { gram: 240, ounce: 8.0, teaspoon: 48 },
-  gram: { cup: 1 / 240, ounce: 0.0353, teaspoon: 0.2 },
-  ounce: { cup: 0.125, gram: 28.35, teaspoon: 6 },
-  teaspoon: { cup: 1 / 48, gram: 5, ounce: 0.167 },
-}
+	cup: { gram: 240, ounce: 8.0, teaspoon: 48 },
+	gram: { cup: 1 / 240, ounce: 0.0353, teaspoon: 0.2 },
+	ounce: { cup: 0.125, gram: 28.35, teaspoon: 6 },
+	teaspoon: { cup: 1 / 48, gram: 5, ounce: 0.167 },
+};
 
 const convertQuantity = (fromUnit) => (toUnit) => (quantity) => {
-  const conversionRate = conversionTable[fromUnit][toUnit];
-  return quantity * conversionRate;
-}
+	const conversionRate = conversionTable[fromUnit][toUnit];
+	return quantity * conversionRate;
+};
 
-const gramsResult = convertQuantity("cup")("gram")(2);
+const gramsResult = convertQuantity('cup')('gram')(2);
 console.log(gramsResult);
 
 const adjustForServings = (baseQuantity) => (newServings) =>
-  baseQuantity * newServings;
+	baseQuantity * newServings;
 
 const servingsResult = adjustForServings(4)(6);
 console.log(servingsResult);
 
 const processIngredient = (baseQuantity, baseUnit, newUnit, newServings) => {
-  const adjustedQuantity = adjustForServings(baseQuantity)(newServings);
-  const convertedQuantity =
-    convertQuantity(baseUnit)(newUnit)(adjustedQuantity);
-  return convertedQuantity.toFixed(2);
+	const adjustedQuantity = adjustForServings(baseQuantity)(newServings);
+	const convertedQuantity =
+		convertQuantity(baseUnit)(newUnit)(adjustedQuantity);
+	return convertedQuantity.toFixed(2);
 };
+
+const ingredientName = document.getElementById('ingredient');
+const ingredientQuantity = document.getElementById('quantity');
+const unitToConvert = document.getElementById('unit');
+const numberOfServings = document.getElementById('servings');
+const recipeForm = document.getElementById('recipe-form');
+const resultList = document.getElementById('result-list');
+
+const units = ['cup', 'gram', 'ounce', 'teaspoon'];
+
+const updateResultsList = () => {
+	resultList.innerHTML = '';
+	units.forEach((item) => {
+		if (item !== unitToConvert.value) {
+			const conversion = processIngredient(
+				Number(ingredientQuantity.value),
+				unitToConvert.value,
+				item,
+				Number(numberOfServings.value),
+			);
+			resultList.innerHTML += `<li>${ingredientName.value}: ${conversion} ${item}</li>`;
+		}
+	});
+};
+
+recipeForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+	updateResultsList();
+});
