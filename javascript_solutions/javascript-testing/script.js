b6936d86153a8472a0e494c3aa956464a326ff71
indigorose <indigorose049@gmail.com>
2026-04-14 22:29:26 +0100

    Update solution

---
+++ javascript_solutions/javascript-testing/script.js
@@ -0,0 +1,33 @@
+let contacts = [
+	{
+		name: 'Maxwell Wright',
+		phone: '(0191) 719 6495',
+		email: 'Curabitur.egestas.nunc@nonummyac.co.uk',
+	},
+	{
+		name: 'Raja Villarreal',
+		phone: '0866 398 2895',
+		email: 'posuere.vulputate@sed.com',
+	},
+	{
+		name: 'Helen Richards',
+		phone: '0800 1111',
+		email: 'libero@convallis.edu',
+	},
+];
+
+let contact = {
+	name: 'Maisie Haley',
+	phone: '0913 531 3030',
+	email: 'risus.Quisque@urna.ca',
+};
+
+// contacts.push(contact);
+// let contactsLength = contacts.length;
+// console.log(contactsLength);
+// console.log(contacts[contactsLength - 1]);
+
+let bool = Boolean(false);
+let bool2 = Boolean(true);
+
+console.log(`${bool + bool2} [${typeof bool}]`);
