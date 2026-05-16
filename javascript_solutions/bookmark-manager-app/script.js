b717ec1f95754bc384d742187b018a697fd82246
Karen Robertson <70444332+indigorose@users.noreply.github.com>
2026-05-16 18:22:27 +0100

    Add display and close function on main and form section

--- javascript_solutions/bookmark-manager-app/script.js
+++ javascript_solutions/bookmark-manager-app/script.js
@@ -1,1 +1,11 @@
-function getBookmarks(bookmarks) {}
+const mainSection = document.getElementById("main-section")
+const formSection = document.getElemenyById("form-section")
+const addBookmarkBtn = document.getElementById("add-bookmark-button")
+function getBookmarks(){
+  const bookmarks = []
+  return bookmarks
+}
+function displayOrCloseForm(){
+mainSection.classList.toggle("hidden")
+formSection.classList.toggle("hidden")
+}
