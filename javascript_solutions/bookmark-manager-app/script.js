f30d07b96f1392ecb0751f94ad5cfca5fd10e58c
Karen Robertson <70444332+indigorose@users.noreply.github.com>
2026-05-17 16:33:38 +0100

    Add displayCloseForm function and addBookmark and closeForm button event listeners

--- javascript_solutions/bookmark-manager-app/script.js
+++ javascript_solutions/bookmark-manager-app/script.js
@@ -1,11 +1,24 @@
 const mainSection = document.getElementById("main-section")
-const formSection = document.getElemenyById("form-section")
+const formSection = document.getElementById("form-section")
 const addBookmarkBtn = document.getElementById("add-bookmark-button")
+const categoryName = document.querySelectorAll(".category-name")
+const categoryDropdown = document.getElementById("category-dropdown")
+const closeFormBtn = document.getElementById("close-form-button")
+
+
 function getBookmarks(){
-  const bookmarks = []
+  const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || []
   return bookmarks
 }
 function displayOrCloseForm(){
-mainSection.classList.toggle("hidden")
+mainSection.classList.toggle("hidden") 
 formSection.classList.toggle("hidden")
 }
+
+addBookmarkBtn.addEventListener("click", ()=>
+{ categoryName.forEach(div => div.textContent = categoryDropdown.options[categoryDropdown.selectedIndex].text
+)
+  displayOrCloseForm()
+})
+
+closeFormBtn.addEventListener("click", displayOrCloseForm)
